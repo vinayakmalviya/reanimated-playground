a36d3164b84c7ccb8b4575dc950be8bc948dbd6f
Vinayak Malviya <vinayak.malviya@somaiya.edu>
2021-04-17 20:26:55 +0530

    :tada: initial commit

---
+++ App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <View style={styles.container}>
+        <Text style={styles.text}>Reanimated Playground</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+  },
+  text: {
+    fontSize: 16,
+    color: '#FFFFFF',
+  },
+});
+
+export default App;
